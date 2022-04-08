c8b922fa9600e703648b00a4ecc7b0cb53f641be
xcambracer <93262233+xcambracer@users.noreply.github.com>
2022-04-08 13:54:24 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+DFRobotMaqueenPlus.I2CInit()
+DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
+basic.pause(1000)
+DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.BLUE)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
